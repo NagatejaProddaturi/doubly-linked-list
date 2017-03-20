ec89cca652c73328ff83c6bf7a44c982c219a0f2
NagatejaProddaturi <nagateja_proddaturi@epam.com>
2017-03-20 16:51:15 +0530

    Update linked-list.js
    6 TCs passed

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,17 +1,53 @@
+"use strict";
 const Node = require('./node');
 
 class LinkedList {
     constructor() {
-        this.length = 0;
-    }
+		this.length=0
+	}
 
-    append(data) {}
+    append(data) {
+			const newN=new Node();
+			newN.data=data;
+			newN.prev=null;
+			newN.next=null;
+		if(this.length==0)	{
+			this._head=newN;
+			this._tail=newN;
+		}
+		else if(this._head!=this._tail){
+			newN.prev=this._tail;
+			this._tail.next=newN;
+			this._tail=newN;
+		}
+		else{
+			this._head.next = newN;
+			newN.prev = this._head;
+			this._tail = newN;
+		}
+		this.length++;
+	}
 
-    head() {}
+    head() {
+		return this._head.data;
+	}
 
-    tail() {}
+    tail() {
+		return this._tail.data;
+	}
 
-    at(index) {}
+    at(index) {
+		if(index==0)	return this._head.data;
+		else{
+			var temp=new Node();
+			temp=this._head;
+			while(index>0)	{
+				temp=temp.next;
+				index--;
+			}	
+			return temp.data;	
+		}
+	}
 
     insertAt(index, data) {}
 
